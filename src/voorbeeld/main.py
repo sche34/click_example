4732aeffd932868d47cfbc3953735966a4b4bf89
sche34 <timo@scheidel.nl>
2024-10-01 19:55:32 +0000

    initial commit

---
+++ src/voorbeeld/main.py
@@ -0,0 +1,19 @@
+import click
+
+@click.command()
+@click.option("--week", default="1", help="Choose the visual number (1, 2 or 3)")
+@click.option("--all", default=False)
+def visual(week, all):
+    possible_options = ['1', '2', '3']
+    if week not in possible_options:
+        raise ValueError('Must be 1, 2 or 3')
+    
+    if week == "1" or all:
+        print('Now it run the class of visual one')
+    if week == '2' or all:
+        print('Now it run the class of visual two')
+    if week == '3' or all:
+        print('Now it run the class of visual three')
+    
+if __name__ == '__main__':
+    visual()
