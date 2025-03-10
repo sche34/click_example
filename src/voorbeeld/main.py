601551ba323d276fa70b34247ba00e040032b090
sche34 <timo@scheidel.nl>
2025-03-10 08:37:27 +0100

    update code in main to 2 visuals

--- src/voorbeeld/main.py
+++ src/voorbeeld/main.py
@@ -4,12 +4,12 @@ from voorbeeld.visual1 import create_visual1
 from voorbeeld.visual2 import create_visual2
 
 @click.command()
-@click.option("--week", default="1", help="Choose the visual number (1, 2 or 3)")
+@click.option("--week", default="1", help="Choose the visual number (1 or 2)")
 @click.option("--all", default=False)
 def visual(week, all):
-    possible_options = ['1', '2', '3']
+    possible_options = ['1', '2']
     if week not in possible_options:
-        raise ValueError('Must be 1, 2 or 3')
+        raise ValueError('Must be 1 or 2')
     
     if week == "1" or all:
         create_visual1()
